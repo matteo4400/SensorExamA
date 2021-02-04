18af9becfadcfe6b0bc90a6c8672a4ed7fc13a39
Axulf <gabrielevezzali99@gmail.com>
2021-02-04 20:26:50 +0100

    gabri 04/02/2021

---
+++ app/src/main/java/com/example/myapplication/LinearAccelerationSensors.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LinearAccelerationSensors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_acceleration_sensors);
+    }
+}
--- app/src/main/java/com/example/myapplication/MotionSensor.java
+++ app/src/main/java/com/example/myapplication/MotionSensor.java
@@ -1,7 +1,8 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MotionSensor extends AppCompatActivity {
@@ -10,5 +11,30 @@ public class MotionSensor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motion_sensor);
+    }
+
+    public void OpenMotionAccelerometerSensors(View view) {
+        Intent intent = new Intent(this, AccelerometerSensors.class);
+        startActivity(intent);
+    }
+
+    public void OpenMotionGravitySensors(View view) {
+        Intent intent = new Intent(this, GravitySensors.class);
+        startActivity(intent);
+    }
+
+    public void OpenMotionGyroscopeSensors(View view) {
+        Intent intent = new Intent(this, GyroscopeSensors.class);
+        startActivity(intent);
+    }
+
+    public void OpenMotionLinearAccelerationSensors(View view) {
+        Intent intent = new Intent(this, LinearAccelerationSensors.class);
+        startActivity(intent);
+    }
+
+    public void OpenMotionStepCounterSensors(View view) {
+        Intent intent = new Intent(this, StepCounterSensors.class);
+        startActivity(intent);
     }
 }
