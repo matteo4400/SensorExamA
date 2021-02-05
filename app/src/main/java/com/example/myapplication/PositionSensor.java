406fcf2e67e6a965bcb930d4f4a02132a58677e6
farelli2 <lory.fari1@gmail.com>
2021-02-05 12:09:44 +0100

    Doppio 05/02/2021

--- app/src/main/java/com/example/myapplication/PositionSensor.java
+++ app/src/main/java/com/example/myapplication/PositionSensor.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PositionSensor extends AppCompatActivity {
 
@@ -11,4 +13,23 @@ public class PositionSensor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_position_sensor);
     }
-}
+
+    public void OpenGeomagneticRotationSensors(View view) {
+        Intent intent = new Intent(this, GeomagneticRotation.class);
+        startActivity(intent);
+    }
+
+    public void OpenMagneticFieldSensors(View view) {
+        Intent intent = new Intent(this, MagneticField.class);
+        startActivity(intent);
+    }
+
+    public void OpenOrientationSensors(View view) {
+        Intent intent = new Intent(this, Orientation.class);
+        startActivity(intent);
+    }
+
+    public void OpenProximitySensors(View view) {
+        Intent intent = new Intent(this, Proximity.class);
+        startActivity(intent);
+    }
